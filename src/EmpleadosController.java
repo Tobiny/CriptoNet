e7368efe1392f9617db597d23401956e0befd5b6
Fernando Chavez <fernandochajim@gmail.com>
2021-05-28 21:47:36 -0500

    Empleados actualizado

--- src/EmpleadosController.java
+++ src/EmpleadosController.java
@@ -3,9 +3,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import src.Empleado;
 
 import java.io.IOException;
 import java.net.URL;
@@ -23,6 +24,22 @@ public class EmpleadosController implements Initializable {
     public Label logoLblH;
     public Label logoLbl;
 
+    public TextField idEmpA;
+    public TextField nomEmpA;
+
+    public ComboBox<String> idEmpM;
+    public TextField nomEmpM;
+
+    public ComboBox<String> nomEmpE;
+    public TextField idEmpE;
+
+    public Button btnEmpA;
+    public Button btnEmpM;
+    public Button btnEmpE;
+
+    public TableView<Empleado> empTable;
+    public TableColumn<Empleado, String> idEmpTable;
+    public TableColumn<Empleado, String> nomEmpTable;
 
     public void changeSceneP(MouseEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
@@ -93,6 +110,15 @@ public class EmpleadosController implements Initializable {
         app_stage.hide();
         app_stage.setScene(home_scene);
         app_stage.show();
+    }
+    public void agregarEmpleado(MouseEvent actionEvent) throws IOException {
+
+    }
+    public void modificarEmpleado(MouseEvent actionEvent) throws IOException {
+
+    }
+    public void eliminarEmpleado(MouseEvent actionEvent) throws IOException {
+
     }
 
     public void exit(MouseEvent actionEvent) throws IOException { System.exit(0); }
