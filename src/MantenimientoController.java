b0c4220f23867ae9f53bc0486903b946a7cfe94a
Fernando Chavez <fernandochajim@gmail.com>
2021-05-31 14:56:32 -0500

    Interfaz mantenimientos terminados

--- src/MantenimientoController.java
+++ src/MantenimientoController.java
@@ -1,12 +1,16 @@
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import src.Mantenimiento;
+import src.NumberTextField;
 
+import javax.xml.soap.Text;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,6 +27,62 @@ public class MantenimientoController implements Initializable {
     public Label logoLblH;
     public Label logoLbl;
 
+    //Menu agregar
+    public TextField idMA;
+    public ComboBox<String> idCA;
+    public DatePicker fechaA;
+    public TextArea idPA;
+    public ComboBox<String> idSerA;
+    public NumberTextField cantSerA;
+    public TextField subTA;
+    public TextField totA;
+    public Button btnAM; //Agregar mantemiento
+    public Button btnAS; //Agregar servicio
+    public Button btnC; //Cancelar mantenimiento
+
+    //Menu modificar
+    public ComboBox<String> idMM;
+    public ComboBox<String> idCM;
+    public DatePicker fechaM;
+    public TextArea idPM;
+    public ComboBox<String> idSerM;
+    public NumberTextField cantSerM;
+    public TextField subTM;
+    public TextField totM;
+    public Button btnMM; //Modificar mantemiento
+    public Button btnMS; //Modificar servicio
+
+    //Menu eliminar
+    public ComboBox<String> idME;
+    public TextField idCE;
+    public DatePicker fechaE;
+    public TextArea idPE;
+    public ComboBox<String> idSerE;
+    public TextField cantSerE;
+    public TextField subTE;
+    public TextField totE;
+    public Button btnEM; //Eliminar mantenimiento
+
+    //Menu consulta general
+    @FXML TableView<Mantenimiento> ventasTable;
+    @FXML TableColumn<Mantenimiento, String> idMant;
+    @FXML TableColumn<Mantenimiento, String> idCli;
+    @FXML TableColumn<Mantenimiento, String> fecha;
+    @FXML TableColumn<Mantenimiento, String> mant;
+    @FXML TableColumn<Mantenimiento, String> idServ;
+    @FXML TableColumn<Mantenimiento, String> cantidadServ;
+    @FXML TableColumn<Mantenimiento, String> subtotal;
+    @FXML TableColumn<Mantenimiento, String> total;
+
+    //Menu consulta individual
+    public ComboBox<String> idMI;
+    public TextField idCI;
+    public DatePicker fechaI;
+    public TextArea idPI;
+    public ComboBox<String> idSerI;
+    public TextField cantSerI;
+    public TextField subTI;
+    public TextField totI;
 
     public void changeSceneP(MouseEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
@@ -94,6 +154,13 @@ public class MantenimientoController implements Initializable {
         app_stage.setScene(home_scene);
         app_stage.show();
     }
+
+    public void agregarMantenimiento(){}
+    public void agregarServicio(){}
+    public void cancelarMantenimiento(){}
+    public void modificarMantenimiento(){}
+    public void modificarServicio(){}
+    public void elinarMantenimiento(){}
 
     public void exit(MouseEvent actionEvent) throws IOException { System.exit(0); }
     public void showLogo(MouseEvent actionEvent) {
