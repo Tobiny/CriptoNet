ad95f33b2b0d333a0e5b06fc8af49c6f9604a0fc
Mim0518 <morenorivera1234@gmail.com>
2021-05-31 22:33:10 -0500

    Empleados Terminado

--- src/EmpleadosController.java
+++ src/EmpleadosController.java
@@ -1,15 +1,22 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import src.Cliente;
+import src.Conexion;
 import src.Empleado;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
 import java.util.ResourceBundle;
 
 public class EmpleadosController implements Initializable {
@@ -45,6 +52,11 @@ public class EmpleadosController implements Initializable {
     public TableColumn<Empleado, String> idEmpTable;
     public TableColumn<Empleado, String> nomEmpTable;
 
+    //Conexion
+    static Conexion c = new Conexion();
+    public static String connectionUrl = c.getConnectionUrl();
+    public ResultSet resultSet;
+
     public void changeSceneP(MouseEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("Productos.fxml"));
@@ -56,7 +68,6 @@ public class EmpleadosController implements Initializable {
         app_stage.setScene(home_scene);
         app_stage.show();
     }
-
     public void changeSceneM(MouseEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("Mantenimientos.fxml"));
@@ -68,7 +79,6 @@ public class EmpleadosController implements Initializable {
         app_stage.setScene(home_scene);
         app_stage.show();
     }
-
     public void changeSceneV(MouseEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("Ventas.fxml"));
@@ -80,7 +90,6 @@ public class EmpleadosController implements Initializable {
         app_stage.setScene(home_scene);
         app_stage.show();
     }
-
     public void changeSceneC(MouseEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("Clientes.fxml"));
@@ -92,7 +101,6 @@ public class EmpleadosController implements Initializable {
         app_stage.setScene(home_scene);
         app_stage.show();
     }
-
     public void changeSceneE(MouseEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("Empleados.fxml"));
@@ -116,13 +124,35 @@ public class EmpleadosController implements Initializable {
         app_stage.show();
     }
     public void agregarEmpleado(MouseEvent actionEvent) throws IOException {
-
+        String sql = "INSERT INTO Empleados VALUES ('"+nomEmpA.getText()+"');";
+        if (c.ejecutarQuery(sql)){
+            JOptionPane.showMessageDialog(null, "El Empleado ha sido agregado", "Agregar Empleados", JOptionPane.INFORMATION_MESSAGE);
+            updates();
+        }else{
+            JOptionPane.showMessageDialog(null, "El Empleado no se agregó", "Agregar Empleados", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
     public void modificarEmpleado(MouseEvent actionEvent) throws IOException {
-
+        String agregarSql = "UPDATE Empleados SET NomEmpleado = '"+nomEmpM.getText()+"' WHERE IdEmpleado = "+getId(idEmpM.getValue());
+        if(JOptionPane.showConfirmDialog(null,"¿Esta seguro que desea modificar este empleado?", "Modificar empleados - Confirmación", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION){
+            if(c.ejecutarQuery(agregarSql)){
+                JOptionPane.showMessageDialog(null, "El empleado ha sido modificado", "Modificar empleados", JOptionPane.INFORMATION_MESSAGE);
+                updates();
+            }else{
+                JOptionPane.showMessageDialog(null, "El empleado ha sido modificado", "Modificar empleados", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }else JOptionPane.showMessageDialog(null, "El empleado ha sido modificado", "Modificar empleados", JOptionPane.INFORMATION_MESSAGE);
     }
     public void eliminarEmpleado(MouseEvent actionEvent) throws IOException {
-
+        String agregarSql = "DELETE FROM Empleados WHERE IdEmpleado = "+getId(idEmpE.getValue());
+        if(JOptionPane.showConfirmDialog(null,"¿Esta seguro que desea eliminar este empleado?", "Eliminar empleados - Confirmación", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION){
+            if(c.ejecutarQuery(agregarSql)){
+                JOptionPane.showMessageDialog(null, "El empleado ha sido eliminado", "Eliminar empleados", JOptionPane.INFORMATION_MESSAGE);
+                updates();
+            }else{
+                JOptionPane.showMessageDialog(null, "El empleado ha sido eliminado", "Eliminar empleados", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }else JOptionPane.showMessageDialog(null, "El empleado ha sido eliminado", "Eliminar empleados", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public void exit(MouseEvent actionEvent) throws IOException { System.exit(0); }
@@ -135,5 +165,89 @@ public class EmpleadosController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        updates();
+    }
+    public void updates(){
+        idEmpM.getItems().clear();
+        idEmpE.getItems().clear();
+        idEmpI.getItems().clear();
+        //IdAgregar
+        try(Connection connection = DriverManager.getConnection(connectionUrl);
+            Statement statement = connection.createStatement();){
+            resultSet = statement.executeQuery("SELECT TOP 1 IdEmpleado FROM Empleados ORDER BY IdEmpleado DESC");
+            while (resultSet.next()){
+                int id = resultSet.getInt("IdEmpleado")+1;
+                idEmpA.setText(Integer.toString(id));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        //Combobox empleados
+        try(Connection connection = DriverManager.getConnection(connectionUrl);
+            Statement statement = connection.createStatement();){
+            resultSet = statement.executeQuery("SELECT IdEmpleado, NomEmpleado FROM Empleados");
+            while (resultSet.next()){
+                idEmpM.getItems().addAll(resultSet.getString("IdEmpleado")+"- "+resultSet.getString("NomEmpleado"));
+                idEmpE.getItems().addAll(resultSet.getString("IdEmpleado")+"- "+resultSet.getString("NomEmpleado"));
+                idEmpI.getItems().addAll(resultSet.getString("IdEmpleado")+"- "+resultSet.getString("NomEmpleado"));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        idEmpM.setOnAction(event -> {
+            try(Connection connection = DriverManager.getConnection(connectionUrl);
+                Statement statement = connection.createStatement();){
+                resultSet = statement.executeQuery("SELECT * FROM Empleados WHERE IdEmpleado = '"+getId(idEmpM.getValue())+"'");
+                while (resultSet.next()){
+                    nomEmpM.setText(resultSet.getString("NomEmpleado"));
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        });
+        idEmpE.setOnAction(event -> {
+            try(Connection connection = DriverManager.getConnection(connectionUrl);
+                Statement statement = connection.createStatement();){
+                resultSet = statement.executeQuery("SELECT * FROM Empleados WHERE IdEmpleado = '"+getId(idEmpE.getValue())+"'");
+                while (resultSet.next()){
+                    nomEmpE.setText(resultSet.getString("NomEmpleado"));
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        });
+        idEmpI.setOnAction(event -> {
+            try(Connection connection = DriverManager.getConnection(connectionUrl);
+                Statement statement = connection.createStatement();){
+                resultSet = statement.executeQuery("SELECT * FROM Empleados WHERE IdEmpleado = '"+getId(idEmpI.getValue())+"'");
+                while (resultSet.next()){
+                    nomEmpI.setText(resultSet.getString("NomEmpleado"));
+                }
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        });
+        ObservableList<Empleado> datosTablaEmp = FXCollections.observableArrayList();
+        try(Connection connection = DriverManager.getConnection(connectionUrl);
+            Statement statement = connection.createStatement();){
+            resultSet = statement.executeQuery("SELECT * FROM Empleados");
+            while (resultSet.next()){
+                datosTablaEmp.add(new Empleado(resultSet.getString("IdEmpleado"), resultSet.getString("NomEmpleado")));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        idEmpTable.setCellValueFactory(new PropertyValueFactory<Empleado, String>("idE"));
+        nomEmpTable.setCellValueFactory(new PropertyValueFactory<Empleado, String>("nom"));
+        empTable.setItems(datosTablaEmp);
+    }
+    //Get id de un combobox
+    public String getId(String textoEnCombo){
+        String idnull = "1";
+        if(textoEnCombo == null) return idnull;
+        else{
+            String[] parts = textoEnCombo.split("-" );
+            return parts[0];
+        }
     }
 }
